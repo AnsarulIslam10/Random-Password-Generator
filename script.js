c39af9c469819b7abbdbaa96c2c935aaa0b5ee24
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-07-14 12:00:47 +0600

    added a new function to generate random password lenght

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const passwordBox = document.getElementById("password");
-const lenght = 12;
-
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789"
@@ -9,6 +7,13 @@ const symbol = "@#$%^&*(){}[]?/-=_+~"
 const allChars = upperCase + lowerCase + number + symbol;
 
 function createPassword(){
+
+    function getRandomLenght(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    let randomLenght = getRandomLenght(8, 16);
+    const lenght = randomLenght;
+
     let password = "";
     password += upperCase[Math.floor(Math.random() * upperCase.length)];
     password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
